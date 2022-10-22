c92dc623fdc87d9c3652f55c1b9db1b7a7f89b4e
Yujinginging <273451@via.dk>
2022-10-22 15:47:40 +0200

    fix the bug of not getting printer name

--- Server/PrinterServer.java
+++ Server/PrinterServer.java
@@ -6,12 +6,7 @@ import java.util.ArrayList;
 public class PrinterServer {
 
     public static void main(String[] args) throws RemoteException {
-        //creating a list of 2 printers and sending the list to the Printer Servant
-        /*ArrayList<Printer> printers = new ArrayList<>();
-        Printer p1 = new Printer("1");
-        Printer p2 = new Printer("2");
-        printers.add(p1);
-        printers.add(p2);*/
+
 
         //
         Registry registry = LocateRegistry.createRegistry(5099);
